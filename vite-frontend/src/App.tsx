25af9db9b01e93c44dd151caea7cf0a415257f5e
Learnedprawn <vineshparalkar@gmail.com>
2025-04-14 20:01:48 +0530

    typescript null checks

--- vite-frontend/src/App.tsx
+++ vite-frontend/src/App.tsx
@@ -6,45 +6,57 @@ import { ConnectButton } from "@rainbow-me/rainbowkit";
 import { useContract } from "./context/ContractInteraction";
 import { BrowserProvider, Contract } from "ethers";
 import Box from "../out/Box.sol/Box.json";
-import { getNumber } from "./context/ContractInteraction";
+// import { getNumber } from "./context/ContractInteraction";
 
 function App() {
   // const [contract, setContract] = useState();
   const [num, setNum] = useState(11);
-  const [inputValue, setInputValue]:any = useState();
+  const [inputValue, setInputValue]: any = useState();
   const contract = useContract();
 
-
   async function getContractNumber() {
-    const number = await contract.getNumber();
+    if (contract) {
+      const number = await contract.getNumber();
+      setNum(number);
+    } else {
+      console.log("No contract");
+    }
+
     // console.log(number);
-    setNum(number);
   }
 
-  async function setContractNumber(inputNumber:any) {
-    const number = await contract.setNumber(inputNumber);
+  async function setContractNumber(inputNumber: any) {
+    if (contract) {
+      const number = await contract.setNumber(inputNumber);
+    } else {
+      console.log("No contract");
+    }
     // console.log(number);
   }
 
   // async function initializeContracts() {
-    // const provider = new BrowserProvider(window.ethereum);
-    // const signer = await provider.getSigner();
+  // const provider = new BrowserProvider(window.ethereum);
+  // const signer = await provider.getSigner();
 
-    // console.log("signer: ", signer);
+  // console.log("signer: ", signer);
 
-    // const contractInstance = new Contract(
-    //   "0x5FbDB2315678afecb367f032d93F642f64180aa3",
-    //   Box.abi,
-    //   signer
-    // );
-    // console.log("contract: ", contractInstance);
-    // setContract(contractInstance);
+  // const contractInstance = new Contract(
+  //   "0x5FbDB2315678afecb367f032d93F642f64180aa3",
+  //   Box.abi,
+  //   signer
+  // );
+  // console.log("contract: ", contractInstance);
+  // setContract(contractInstance);
   // }
 
   return (
     <>
       <button onClick={getContractNumber}>{num}</button>
-      <input onChange={(e) => {setInputValue(e.target.value)}}></input>
+      <input
+        onChange={(e) => {
+          setInputValue(e.target.value);
+        }}
+      ></input>
       <button onClick={() => setContractNumber(inputValue)}>set</button>
       {/* <button onClick={initializeContracts}>initialize</button> */}
       <ConnectButton></ConnectButton>
--- vite-frontend/src/context/ContractInteraction.tsx
+++ vite-frontend/src/context/ContractInteraction.tsx
@@ -1,16 +1,21 @@
 import { createContext, useContext, useEffect, useState } from "react";
-import { ethers, BrowserProvider } from "ethers";
+import { BrowserProvider, Contract } from "ethers";
 import { useAccount } from "wagmi";
 import Box from "../../out/Box.sol/Box.json";
 
-const contractContext = createContext();
+const contractContext = createContext<Contract | null>(null);
 
-export function useContract() {
+export function useContract(): Contract | null {
   return useContext(contractContext);
 }
 
-export function ContractProvider({ children }) {
-  const [contract, setContract] = useState();
+interface ContractProviderProps {
+  children: React.ReactNode;
+}
+
+
+export function ContractProvider({ children }: ContractProviderProps) {
+  const [contract, setContract] = useState<Contract | null>(null);
   useEffect(() => {
     async function createContract() {
       if (window.ethereum) {
@@ -18,7 +23,7 @@ export function ContractProvider({ children }) {
         // const provider = new ethers.providers.Web3Provider(window.ethereum);
         const provider = new BrowserProvider(window.ethereum); // ✅ Ethers v6
         const signer = await provider.getSigner();
-        const contractInstance = new ethers.Contract(
+        const contractInstance = new Contract(
           "0x5FbDB2315678afecb367f032d93F642f64180aa3",
           Box.abi,
           signer
@@ -36,10 +41,10 @@ export function ContractProvider({ children }) {
   );
 }
 
-export async function getNumber() {
-  return await contract.getNumber();
-}
+// export async function getNumber() {
+//   return await contract.getNumber();
+// }
 
-export async function setNumber(inputNumber) {
-  return await contract.setNumber(inputNumber);
-}
+// export async function setNumber(inputNumber) {
+//   return await contract.setNumber(inputNumber);
+// }
